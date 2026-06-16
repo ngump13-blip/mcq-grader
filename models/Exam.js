ebd16010793b8095d01955966f4465ba3d6c2dfa
ngump13-blip <ngump13@gmail.com>
2026-06-16 12:11:24 -0700

    Delete models directory

--- models/Exam.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-
-const examSchema = new mongoose.Schema({
-  title: String,
-  answerKey: [String]
-});
-
-module.exports = mongoose.model("Exam", examSchema);
